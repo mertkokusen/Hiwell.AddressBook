be269ac6e8cda00027b90cfe8a04d8d4e689a163
mertkokusen <mertkokusen@hotmail.com>
2021-02-21 14:34:42 +0300

    redirect index.html to swagger/index.html

--- Hiwell.AddressBook.API/Startup.cs
+++ Hiwell.AddressBook.API/Startup.cs
@@ -47,9 +47,14 @@ namespace Hiwell.AddressBook.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hiwell.AddressBook.API v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hiwell.AddressBook.API v1");
+                c.RoutePrefix = string.Empty;
+            });
+
 
             app.UseHttpsRedirection();
 
